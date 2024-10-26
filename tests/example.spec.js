f5be7e53b348343f77f5f715c04b95b856a6d62b
Nick Hummel <NckHmml@users.noreply.github.com>
2024-10-26 20:41:18 +0200

    Fixed timeout, test per id

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,35 +1,55 @@
 // @ts-check
-const { test } = require('@playwright/test');
-const { readFile } = require("fs/promises");
+const { test } = require("@playwright/test");
+const { readFileSync } = require("fs");
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-test("default", async ({ page }) => {
-  const giftCode = await readFile("./giftcode.txt").then(b => b.toString("utf8"));
-  const playerIds = (await readFile("./players.txt").then(b => b.toString("utf8"))).trim().split(/\n|,/).map(x => x.trim());
+const giftCode = readFileSync("./giftcode.txt").toString("utf-8");
+const playerIds = readFileSync("./players.txt")
+  .toString("utf8")
+  .trim()
+  .split(/\n|,/)
+  .map(x => x.trim())
+  .filter((x, i, a) => a.indexOf(x) === i);
+
+test.describe(`Running "${giftCode}"`, () => {
   for (const playerId of playerIds) {
     if (!playerId) continue;
+    
+    test(`player "${playerId}"`, async ({page}) => {
 
-    await page.goto("https://wos-giftcode.centurygame.com/");
+      await page.goto("https://wos-giftcode.centurygame.com/");
+    
+      await page.waitForSelector("[placeholder='Player ID']");
+      await page.waitForTimeout(random(750, 1500));
   
-    await page.waitForSelector("[placeholder='Player ID']");
-    await page.waitForTimeout(random(750, 1500));
-
-    await page.locator("[placeholder='Player ID']").pressSequentially(playerId, { delay: random(60, 80) });
-
-    await page.locator(".login_btn").click();
-    await page.waitForSelector(".img.avatar");
-    await page.waitForTimeout(random(200, 500));
-
-    await page.locator("[placeholder='Enter Gift Code']").pressSequentially(giftCode, { delay: random(60, 80) });
-    await page.waitForTimeout(random(200, 500));
+      await page.locator("[placeholder='Player ID']").pressSequentially(playerId, { delay: random(60, 80) });
   
-    await page.locator(".exchange_btn").click();
-    await page.waitForSelector(".message_modal");
-    await page.waitForTimeout(random(200, 500));
+      await page.locator(".login_btn").click();
+      let failed = false;
+      try {
+        await page.waitForSelector(".img.avatar", { timeout: 2000 });
+        await page.waitForTimeout(random(200, 500));
+    
+        await page.locator("[placeholder='Enter Gift Code']").pressSequentially(giftCode, { delay: random(60, 80) });
+        await page.waitForTimeout(random(200, 500));
+      
+        await page.locator(".exchange_btn").click();
+      } catch {
+        failed = true;
+      }
+      await page.waitForSelector(".message_modal");
+      await page.waitForTimeout(random(200, 500));
+  
+      await page.locator(".message_modal .confirm_btn").click();
 
-    await page.locator(".message_modal .confirm_btn").click();
+      if (failed) {
+        throw `Avatar not found after 2 seconds for player "${playerId}"`;
+      }
+
+    });
   }
+
 });
